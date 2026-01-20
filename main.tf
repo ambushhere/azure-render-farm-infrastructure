1d3709998a649caffb68595cb571eb6464425e06
ambushhere <himynameisroo@gmail.com>
2026-01-20 16:21:20 +0100

    Fix: move tags inside the resource block

--- main.tf
+++ main.tf
@@ -51,9 +51,43 @@ resource "random_string" "suffix" {
 }
 
 # 6. Linux Virtual Machine Scale Set: The core compute engine of the farm
-# This allows the farm to scale capacity based on rendering demand
 resource "azurerm_linux_virtual_machine_scale_set" "render_nodes" {
   name                = "render-nodes-set"
   resource_group_name = azurerm_resource_group.render_rg.name
   location            = azurerm_resource_group.render_rg.location
-  sku                 = var.vm_sku # Defined in
+  sku                 = var.vm_sku # Defined in variables.tf
+  instances           = 2
+  admin_username      = "renderadmin"
+
+  admin_password                  = "P@ssw0rd1234!" 
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  network_interface {
+    name    = "render-nic"
+    primary = true
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.render_subnet.id
+    }
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  # Тэги должны быть ВНУТРИ фигурных скобок ресурса azurerm_linux_virtual_machine_scale_set
+  tags = {
+    Environment = "Development"
+    Project     = "RenderFarm"
+    CostCenter  = "3D-Production"
+  }
+} 
